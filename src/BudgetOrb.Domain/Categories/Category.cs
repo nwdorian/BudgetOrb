46c762c24bded0f4ef176b266d2640db1abfeccb
nwdorian <neuwirthdorian@gmail.com>
2026-01-28 19:49:01 +0100

    feat: add Category and Transaction entities

---
+++ src/BudgetOrb.Domain/Categories/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.Transactions;
+
+namespace BudgetOrb.Domain.Categories;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public ICollection<Transaction> Transactions { get; init; } = [];
+}
---
+++ src/BudgetOrb.Domain/Transactions/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetOrb.Domain.Transactions;
+
+public class Transaction
+{
+    public Guid Id { get; set; }
+    public Guid CategoryId { get; set; }
+    public decimal Amount { get; set; }
+    public string? Comment { get; set; }
+    public DateTime Date { get; set; }
+}
